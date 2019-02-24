7f486eddd51e8d56f7faed20cafe9fb54db696a7
anantkes19 <anantkes@colby.edu>
2019-02-24 13:34:39 -0500

    Shooting and planes

--- Assets/controlMe.cs
+++ Assets/controlMe.cs
@@ -5,20 +5,49 @@ using UnityEngine.Networking;
 
 public class controlMe : NetworkBehaviour
 {
+    public GameObject bulletPrefab;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         if (!isLocalPlayer)
             return;
-        var x = Input.GetAxis("Horizontal") * 0.1f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.down * 5);
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.up * 5);
+
         var z = Input.GetAxis("Vertical") * 0.1f;
 
-        transform.Translate(x, 0, z);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
+
+        transform.Translate(0, 0, z);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        //GetComponent<MeshRenderer>().material.color = Color.red;
+        Camera.main.transform.parent = this.transform;
+        Camera.main.transform.position = new Vector3(0f, 5, -15f);
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate( bulletPrefab, transform.position + transform.forward * 6f, Quaternion.identity);
+
+        // make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = transform.forward * 15;
+
+        NetworkServer.Spawn(bullet);
+
+        // make bullet disappear after 2 seconds
+        Destroy(bullet, 4.0f);
     }
 }
---
+++ Assets/shoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject.transform.root;
+        var hitPlayer = hit.GetComponent<controlMe>();
+        if (hitPlayer != null)
+        {
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(10);
+
+            Destroy(gameObject);
+        }
+    }
+}
