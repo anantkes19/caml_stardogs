b902c441678e42f9692011369c357ca134238ed2
anantkes19 <anantkes@colby.edu>
2019-02-24 11:03:54 -0500

    Multiplayer working!

---
+++ Assets/controlMe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class controlMe : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+        var x = Input.GetAxis("Horizontal") * 0.1f;
+        var z = Input.GetAxis("Vertical") * 0.1f;
+
+        transform.Translate(x, 0, z);
+    }
+}
